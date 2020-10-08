dbb95d0cb39bdcce55889c02ef69960c542b855c
pawar64 <pawar64@avaya.com>
2020-10-08 19:49:40 +0530

    First commit

---
+++ HibernateSessionFactoryCreation/src/main/java/com/infotech/client/ClientTest.java
@@ -0,0 +1,24 @@
+package com.infotech.client;
+
+import org.hibernate.Session;
+
+import com.infotech.util.HibernateUtil;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+
+
+		try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+			
+			String SQL = "SELECT VERSION ();";
+			 String result =  (String)session.createNativeQuery(SQL).getSingleResult();
+			 System.out.println("Mysql Version is :::");
+			 System.out.println(result);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
